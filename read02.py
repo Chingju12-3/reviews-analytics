3f2ea9d9b0bfcbd02bdaa7b6431e7dac65cf7873
Ching Wu <j2u0d3y1@gmail.com>
2021-06-27 17:52:08 +0800

    add filtering

---
+++ read02.py
@@ -0,0 +1,23 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1 # count = count + 1
+		if count % 1000 == 0: # % = 是用來求餘數
+			print(len(data))
+print('檔案讀取完成，總共有', len(data), '筆資料') # len = 長度
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('留言的平均長度為', sum_len/len(data))
+
+new = []
+for d in data: # for loop = 把清單中東西一個一個拿出來
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
